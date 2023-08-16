38dfbb8dd8d83e7482d2d35635efede07147bf85
Andrej Tejbus <andrejado40@gmail.com>
2023-08-16 15:00:13 +0200

    bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import './App.css';
 import schema from './schemas/schema_2.json'
@@ -7,18 +7,23 @@ function App() {
   const initialFields = schema
 
   const [leftFields, setLeftFields] = useState(initialFields);
-  const [rightFieldGroups, setRightFieldGroups] = useState([[]]);
-  const [marks, setMarks] = useState([[]])
+  const [marks, setMarks] = useState([[[]]])
   const [activeMark, setActiveMark] = useState(0)
-
+  const [rightFieldGroups, setRightFieldGroups] = useState(marks[activeMark]);
+  
+  const update = () => {
+    const updatedMarks = [...marks];
+    updatedMarks[activeMark] = rightFieldGroups; 
+    setMarks(updatedMarks);     
+  }
   const onDragEnd = (result) => {
+    update()
     if (!result.destination) return;
   
     const sourceList = result.source.droppableId;
     const destinationList = result.destination.droppableId;
   
     if (sourceList === destinationList) {
-      // Presúvame v rámci toho istého zoznamu
       if (sourceList === 'left-list') {
         const updatedFields = Array.from(leftFields);
         const [movedField] = updatedFields.splice(result.source.index, 1);
@@ -34,7 +39,6 @@ function App() {
         setRightFieldGroups(updatedGroups);
       }
     } else {
-      // Presúvame z jedného zoznamu do druhého
       const sourceFields = sourceList === 'left-list' ? leftFields : rightFieldGroups[parseInt(sourceList.split('-')[1])];
       const destinationFields = destinationList === 'left-list' ? leftFields : rightFieldGroups[parseInt(destinationList.split('-')[1])];
   
@@ -70,12 +74,75 @@ function App() {
     const updatedMarks = [...marks];
     updatedMarks.splice(index, 1);
     setMarks(updatedMarks);
-
+     console.log(index)
     if (index === activeMark) {
       setActiveMark(0); 
     }
   }
   };
+  
+
+  const changeMark = (index) => {
+    /*
+    console.log("Start\nMarks")
+    console.log(marks)
+    console.log("right field groups")
+    console.log(rightFieldGroups)*/
+
+    const updatedMarks = [...marks];
+    updatedMarks[activeMark] = rightFieldGroups; 
+    setMarks(updatedMarks); 
+    
+    setActiveMark(index); 
+    setRightFieldGroups(marks[index]);
+
+    /*
+    console.log("End\nMarks")
+    console.log(marks)
+    console.log("right field groups")
+    console.log(rightFieldGroups)*/
+  };
+
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  useEffect(() => {
+    if (buttonClicked) {
+      changeMark(marks.length - 1);
+      setButtonClicked(false); 
+    }
+  }, [buttonClicked, marks]);
+
+  const generate = () => {
+    const fieldData = marks.map((mark) =>
+      mark.map((group) =>
+        group.map((field) => [
+          {
+            field: field.field
+          }
+        ])
+      )
+    );
+  
+    const jsonData = JSON.stringify(fieldData, null, 2);
+  
+    // Vytvořte Blob objekt z JSON dat
+    const blob = new Blob([jsonData], { type: "application/json" });
+  
+    // Vytvořte URL objekt pro Blob
+    const url = URL.createObjectURL(blob);
+  
+    // Vytvořte odkaz pro stažení souboru
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "fields.json";
+    a.click();
+  
+    // Uvolnění URL objektu
+    URL.revokeObjectURL(url);
+  };
+  
+  
+  
 
   return (
     <div className="App">
@@ -185,8 +252,12 @@ function App() {
             backgroundColor: "yellow",
             width: "20%",
             minHeight: "calc(100vh - 20px)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
             
           }}>
+            <div>
             {marks.map((mark,i) => {
               let active = activeMark===i ? "bold" : "normal";
               let border = activeMark===i ? "3px solid black" : "1px solid black";
@@ -208,7 +279,7 @@ function App() {
                       fontWeight: active,
                       margin: "20px 0 5px 0"
                     }}
-                    onClick={() => setActiveMark(i)}
+                    onClick={() => changeMark(i)}
                   >Záložka {i+1}</h3>
                   <h3
                   style={{
@@ -229,10 +300,27 @@ function App() {
               justifyContent: "center",
               marginTop: "30px"
             }}>
-            <button onClick={() => setMarks(prevMarks => [...prevMarks, []])}>
+            <button onClick={() => {
+              setMarks(prevMarks => [...prevMarks, []])
+              setButtonClicked(true)              
+            }}>
               Pridať záložku
             </button>
             </div>
+          </div>
+          <div
+            style={{
+              textAlign: "center",
+            }}
+          >
+            <button onClick={generate}
+              style={{
+                margin: "0 0 20px 0"
+              }}
+            >
+              GENEROVAŤ
+            </button>
+          </div>
           </div>
         </div>
       </DragDropContext>
